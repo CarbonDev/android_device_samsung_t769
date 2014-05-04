2d302290f9ea35e6be53406175de3be94bb95270
Rabih <somcom3@gmail.com>
2014-05-03 21:30:53 -0400

    Remove overlay
    Simon- "should fix app"
    
    Change-Id: I562d8af73d5db8b691c2ad36ee4d04f87841453a

--- carbon.mk
+++ carbon.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/samsung/t769/full_t769.mk)
 # Inherit some common Carbon stuff.
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T769 TARGET_DEVICE=SGH-T769 BUILD_FINGERPRINT="samsung/SGH-T769/SGH-T769:4.1.2/JZO54K/UVLH5:user/release-keys" PRIVATE_BUILD_DESC="SGH-T769-user 4.1.2 JZO54K UVLH5 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=SGH-T769 BUILD_FINGERPRINT="samsung/SGH-T769/SGH-T769:4.1.2/JZO54K/UVLH5:user/release-keys" PRIVATE_BUILD_DESC="SGH-T769-user 4.1.2 JZO54K UVLH5 release-keys"
 
 PRODUCT_NAME := carbon_t769
 PRODUCT_DEVICE := t769
